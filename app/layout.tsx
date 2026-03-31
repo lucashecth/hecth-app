311c3631f8ed5bc623c6170957203c75e5f7495e
Lucas Hecth <lucas.hecth@gmail.com>
2026-03-31 12:18:01 -0300

    Teste notificações por push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // <-- IMPORTAÇÃO DO SCRIPT DO NEXT.JS
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,10 +10,10 @@ const inter = Inter({ subsets: ["latin"] });
 // CONFIGURAÇÃO DE CORES E TELA (UX NATIVA)
 // ==========================================
 export const viewport: Viewport = {
-  themeColor: "#ef3340", // Cor da barra de status no Android
+  themeColor: "#ef3340",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1, // Impede o zoom na tela, dando cara de App nativo
+  maximumScale: 1, 
   userScalable: false,
 };
 
@@ -22,11 +23,11 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   title: "HECTH.",
   description: "CT Hecth Futevôlei.",
-  manifest: "/manifest.json", // <-- A MÁGICA DO ANDROID AQUI
+  manifest: "/manifest.json", 
   icons: {
-    icon: "/icon.png", // Favicon padrão
+    icon: "/icon.png", 
     shortcut: "/icon.png",
-    apple: "/apple-touch-icon.png", // Ícone vital para iPhone
+    apple: "/apple-touch-icon.png", 
   },
 };
 
@@ -38,6 +39,26 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} bg-black text-white antialiased`}>
+        
+        {/* ========================================== */}
+        {/* CÓDIGO OFICIAL DO ONESIGNAL */}
+        {/* ========================================== */}
+        <Script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" strategy="afterInteractive" />
+        <Script id="onesignal-init" strategy="afterInteractive">
+          {`
+            window.OneSignalDeferred = window.OneSignalDeferred || [];
+            OneSignalDeferred.push(async function(OneSignal) {
+              await OneSignal.init({
+                appId: "8a2576f5-33b1-4791-be60-2c0a5444b633",
+                safari_web_id: "web.onesignal.auto.246fdfe2-a404-4d40-aa8a-d2b211d431d5",
+                notifyButton: {
+                  enable: true,
+                },
+              });
+            });
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
